53c68db20522a4e8a0f9991994414077944823e8
liboyuan08 <2020079085@139.com>
2026-04-17 00:21:56 +0800

    [UPDATE]高二下期中考试 古诗范围所有篇目

--- scripts/poem-content/ori2full.mjs
+++ scripts/poem-content/ori2full.mjs
@@ -26,7 +26,7 @@ const __dirname = path.dirname(__filename);
 /** 强制更新列表，用于 --force 模式下指定必须覆盖的诗文名称 */
 const forceList = {
     junior: [],
-    senior: ["兰亭集序"]
+    senior: ["陈情表","项脊轩志","兰亭集序","归去来兮辞并序","种树郭橐驼传","石钟山记"]
 };
 
 let GLOBAL_TAG_LIST = [];
