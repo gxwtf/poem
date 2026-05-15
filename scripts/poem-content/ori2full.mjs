c220c2bcf06a4df56aa2dac19f9cad25d7cbd46a
liboyuan08 <2020079085@139.com>
2026-05-15 21:46:45 +0800

    [UPDATE](烛之武退秦师)[翻译]

--- scripts/poem-content/ori2full.mjs
+++ scripts/poem-content/ori2full.mjs
@@ -26,7 +26,7 @@ const __dirname = path.dirname(__filename);
 /** 强制更新列表，用于 --force 模式下指定必须覆盖的诗文名称 */
 const forceList = {
     junior: [],
-    senior: ["庖丁解牛"]
+    senior: ["烛之武退秦师"]
 };
 
 let GLOBAL_TAG_LIST = [];
