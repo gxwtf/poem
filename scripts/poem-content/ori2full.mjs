3add3712e748337fb9ced63642cb9910bf1be9fe
liboyuan08 <2020079085@139.com>
2026-06-02 23:14:24 +0800

    [UPDATE](蜀道难)[翻译、背景]

--- scripts/poem-content/ori2full.mjs
+++ scripts/poem-content/ori2full.mjs
@@ -26,7 +26,7 @@ const __dirname = path.dirname(__filename);
 /** 强制更新列表，用于 --force 模式下指定必须覆盖的诗文名称 */
 const forceList = {
     junior: [],
-    senior: ["五代史伶官传序"]
+    senior: ["蜀道难"]
 };
 
 let GLOBAL_TAG_LIST = [];
